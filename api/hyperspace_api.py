4bb95e805506b55733c32aa1bcfb2e63afd131bf
musingsole <musingsole@gmail.com>
2021-04-28 23:35:46 -0400

    Working bounty submission endpoint

--- api/hyperspace_api.py
+++ api/hyperspace_api.py
@@ -2,9 +2,12 @@ from time import sleep
 import json
 from dataclasses import dataclass
 from urllib.parse import unquote_plus
+from base64 import b64decode
 
 from LambdaPage import LambdaPage
 from murdaws import murd_ddb as mddb
+from requests_toolbelt.multipart import MultipartDecoder
+import s3
 
 
 mddb.ddb_murd_prefix = "musbb_murd_"
@@ -23,7 +26,7 @@ class Bounty:
     Benefactor: str
     Contact: str
     TemplateProject: str
-    ProjectDescription: str
+    BountyDescription: str
     ReferenceMaterial: list
 
     def to_dict(self):
@@ -32,7 +35,9 @@ class Bounty:
 
     @classmethod
     def from_m(cls, m):
-        return cls(**{k: v for k, v in m.items() if k not in [mddb.group_key, mddb.sort_key]})
+        bounty = cls(**{k: v for k, v in m.items() if k not in [mddb.group_key, mddb.sort_key]})
+        bounty.ReferenceMaterial = json.loads(bounty.ReferenceMaterial)
+        return bounty
 
 
 def get_bounty_board(event):
@@ -49,12 +54,12 @@ def handle_get_bounty(event):
     return 200, get_bounty(bounty_name).to_dict()
 
 
-def new_bounty(event):
+def write_bounty(new_bounty):
     # Update server data
-    new_bounty = Bounty(**json.loads(event['body']))
     try:
         get_bounty(new_bounty.BountyName)
-        return 403, f"Bounty {new_bounty.BountyName} already exists"
+        print(f"Bounty {new_bounty.BountyName} already exists")
+        return False
     except IndexError:
         pass
 
@@ -62,11 +67,19 @@ def new_bounty(event):
                      mddb.sort_key: new_bounty.BountyName},
                   **new_bounty.to_dict()}])
     # TODO: read back after write to test for success
-    written_bounty = Bounty.from_m(murd.read_first(group="bounty", sort=new_bounty.BountyName))
-    if written_bounty == new_bounty:
-        return 200
+    recovered_bounty = Bounty.from_m(murd.read_first(group="bounty", sort=new_bounty.BountyName))
+    if recovered_bounty == new_bounty:
+        return True
     else:
-        return 503
+        print("Recovered bounty does not match intended value")
+        print(recovered_bounty)
+        print(new_bounty)
+        return False
+
+
+def handle_new_bounty(event):
+    bounty_defn = Bounty(**json.loads(event['body']))
+    return 200 if write_bounty(bounty_defn) else 503
 
 
 def serve_bounty_submission(event):
@@ -78,28 +91,83 @@ def serve_bounty_submission(event):
 <body>
   Here's the form
   <form enctype="multipart/form-data" action="/rest/bounty_form" method="post">
-    Bounty: <input type="float" name="bounty_amount"><br>
-    Bounty Name: <input type="text" name="bounty_name"><br>
-    Benefactor: <input type="text" name="benefactor"><br>
-    Contact Benefactor at: <input type="text" name="benefactor_contact"><br>
-    Template Project: <input type="text" name="template_project"><br>
-    Project Description: <textarea rows="5" cols="50" name="bounty_description"></textarea><br>
-    Reference Material: <input type="file" name="reference_material" multiple><br>
+    Bounty: <input type="float" name="Bounty"><br>
+    Bounty Name: <input type="text" name="BountyName"><br>
+    Benefactor: <input type="text" name="Benefactor"><br>
+    Contact at: <input type="text" name="Contact"><br>
+    Template Project: <input type="text" name="TemplateProject"><br>
+    Bounty Description: <textarea rows="5" cols="50" name="BountyDescription"></textarea><br>
+    Reference Material: <input type="file" name="ReferenceMaterial" multiple><br>
     <input type="submit"><br>
 </body>
 """
 
 
+def get_form_name(part):
+    defn = part.headers[b'Content-Disposition'].decode()
+    return defn[defn.find(" name="):].split(";")[0].split("=")[1].replace('"', '')
+
+
+def get_refmat_filename(part):
+    defn = part.headers[b'Content-Disposition'].decode()
+    return defn[defn.find(" filename="):].split(";")[0].split("=")[1].replace('"', '')
+
+
+def write_refmat_to_s3(bounty_name, refmat_name, refmat_content):
+    s3_path = f"bountyboard/{bounty_name}/{refmat_name}"
+    s3.write(s3_path, refmat_content)
+
+
+bounty_name_map = {
+    "bounty_amount": "Bounty",
+    "bounty_name": "BountyName",
+    "benefactor": "Benefactor",
+    "benefactor_contact": "Contact",
+    "template_project": "TemplateProject",
+    "bounty_description": "BountyDescription",
+    "reference_material": "ReferenceMaterial",
+}
+
+
 def submit_bounty_form(event):
-    # Handle multipart form
-    return 200
+    new_bounty_defn = {}
+    refmat_material = {}
+
+    content_type = event['headers']['content-type']
+    form_data = b64decode(event['body'])
+
+    multipart_decoder = MultipartDecoder(content=form_data, content_type=content_type)
+    for part in multipart_decoder.parts:
+        form_name = get_form_name(part)
+        form_name = form_name if form_name not in bounty_name_map else bounty_name_map[form_name]
+        if form_name == 'ReferenceMaterial':
+            filename = get_refmat_filename(part)
+            new_bounty_defn['ReferenceMaterial'] = [filename]
+            refmat_material[filename] = b64decode(part.content)
+        else:
+            new_bounty_defn[form_name] = part.content.decode()
+
+    new_bounty = Bounty(**new_bounty_defn)
+    if not write_bounty(new_bounty):
+        return 503, 'Failed to create bounty'
+
+    try:
+        for refmat, refmat_content in refmat_material.items():
+            write_refmat_to_s3(new_bounty.BountyName, refmat, refmat_content)
+    except:
+        print("Deleting bounty")
+        murd.delete([{mddb.group_key: "bounty", mddb.sort_key: new_bounty.BountyName, **new_bounty_defn}])
+        raise
+    print(murd.read(group="bounty", sort=new_bounty.BountyName))
+
+    return 200, ''
 
 
 def build_page():
     page = LambdaPage()
     page.add_endpoint(method="get", path="/rest/bounty_form", func=serve_bounty_submission, content_type="text/html")
     page.add_endpoint(method="post", path="/rest/bounty_form", func=submit_bounty_form)
-    page.add_endpoint(method="post", path="/rest/bountyboard", func=new_bounty)
+    page.add_endpoint(method="post", path="/rest/bountyboard", func=handle_new_bounty)
     page.add_endpoint(method="get", path="/rest/bountyboard/{bounty_name}", func=handle_get_bounty)
     page.add_endpoint(method="get", path="/rest/bountyboard", func=get_bounty_board)
     return page
--- api/s3.py
+++ api/s3.py
@@ -3,7 +3,7 @@ from io import BytesIO
 import boto3
 
 
-bucket = "makurspace"
+bucket = "makurspace-static-assets"
 
 
 def retrieve(s3_path):
@@ -25,7 +25,7 @@ def retrieve_presigned_url(s3_path):
     s3c = boto3.client("s3")
     url = s3c.generate_presigned_url('get_object',
                                      ExpiresIn=1800,
-                                     Params={'Bucket': 'musingsole',
+                                     Params={'Bucket': bucket,
                                              'Key': s3_path})
     return url
 
@@ -37,10 +37,13 @@ def list_contents(s3_path):
 
 
 def write(s3_path, content):
-    print(f"Writting {s3_path}")
-    s3b = boto3.resource("s3").Bucket("musingsole")
-    content_bytes = BytesIO(content.encode("utf-8"))
-    s3b.upload_fileobj(content_bytes, s3_path)
+    print(f"Writing {bucket}/{s3_path}")
+    content_bytes = BytesIO(content)
+    boto3.client("s3").upload_fileobj(
+        Fileobj=content_bytes,
+        Bucket=bucket,
+        Key=s3_path,
+        ExtraArgs={'ACL':'public-read'})
 
 
 def write_json(s3_path, content):
@@ -50,5 +53,5 @@ def write_json(s3_path, content):
 
 def delete(s3_path):
     print(f"Deleting {s3_path}")
-    s3b = boto3.resource("s3").Bucket("musingsole")
+    s3b = boto3.resource("s3").Bucket(bucket)
     s3b.delete_objects(Delete={"Objects": [{"Key": s3_path}]})
