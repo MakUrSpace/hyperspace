f4c02eed3c454d0cc8cc5e2d24680362b20b36ba
unknown <ssmith01850@gmail.com>
2021-12-10 12:01:48 -0500

    0001 - Currently working on getting the test div tag to render the html

--- hyperspace/justpy_template/navbar.py
+++ hyperspace/justpy_template/navbar.py
@@ -1,10 +1,21 @@
 import justpy as jp
 
-class MUS_Navbar(jp.Div):
+class MUS_Navbar():
 
-    def navbar(self, request):
+    def navbar():
         wp = jp.WebPage()
-        navbar_div = jp.Div(classes='bg-black', a=wp)
+        navbar_html = jp.parse_html("""
+        <head>
+            <title>MUS Test</title>
+        </head>
+            <div>
+            <p>Test</p>
+            </div>
+            """, a=wp)
+        for i in navbar_html.commands:
+            print(i)
+            jp.Div(text=i, classes='font-mono ml-2', a=wp)
+        print()
         return wp
 
-jp.justpy(MUS_Navbar().navbar)
+jp.justpy(MUS_Navbar)
