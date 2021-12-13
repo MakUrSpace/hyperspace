26605a76e66abddd16cd899b83a98f0ff2f81c9a
ssmith01850 <ssmith01850@gmail.com>
2021-12-13 13:52:21 -0500

    0006 - Ripped some example CSS from the JustPy tutorial and incorporated it into the navbar html. It worked out as expected, but still ugly.

--- hyperspace/justpy_template/navbar.py
+++ hyperspace/justpy_template/navbar.py
@@ -4,6 +4,7 @@ class MUS_Navbar(jp.Div):
 
     def navbar(self, request):
         wp = jp.WebPage()
+        navbar_class_html = "m-2 bg-gray-200 border-2 border-gray-200 rounded w-64 py-2 px-4 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"
         navbar_html = jp.parse_html("""
             <div>
                 <ul>
@@ -11,11 +12,10 @@ class MUS_Navbar(jp.Div):
                     <li><input type="search" placeholder="Search Bounties"></li>
                     <li>Home</li>
                     <li>Bounty Board</li>
-                    <li>Sign up / Login</li>
+                    <li>Sign Up / Login</li>
                 </ul>
             </div>
-            """, a=wp)
-        #print()
+            """, a=wp, classes=navbar_class_html)
         return wp
 
 jp.justpy(MUS_Navbar().navbar)
