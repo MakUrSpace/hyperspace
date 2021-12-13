8beb3ca3a9a1dd8555230d8e2c017549873b5c5c
ssmith01850 <ssmith01850@gmail.com>
2021-12-13 14:43:05 -0500

    0007 - Ripped some sample code that incorporates Tailwind CSS. Now the navbar looks better

--- hyperspace/justpy_template/navbar.py
+++ hyperspace/justpy_template/navbar.py
@@ -4,18 +4,49 @@ class MUS_Navbar(jp.Div):
 
     def navbar(self, request):
         wp = jp.WebPage()
-        navbar_class_html = "m-2 bg-gray-200 border-2 border-gray-200 rounded w-64 py-2 px-4 text-gray-700 focus:outline-none focus:bg-white focus:border-purple-500"
         navbar_html = jp.parse_html("""
-            <div>
-                <ul>
-                    <li>MakUrSpace</li>
-                    <li><input type="search" placeholder="Search Bounties"></li>
-                    <li>Home</li>
-                    <li>Bounty Board</li>
-                    <li>Sign Up / Login</li>
+            <nav class="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-500 mb-3">
+            <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
+                <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+                <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+                    pink Color
+                </a>
+                <button class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
+                    <span class="block relative w-6 h-px rounded-sm bg-white"></span>
+                    <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+                    <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+                </button>
+                </div>
+                <div class="lg:flex flex-grow items-center" id="example-navbar-warning">
+                <ul class="flex flex-col lg:flex-row list-none mr-auto">
+                    <li class="nav-item">
+                    <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                        <i class="fab fa-facebook-square text-lg leading-lg text-white opacity-75" /> 
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                        <i class="fab fa-twitter text-lg leading-lg text-white opacity-75" /> <span class="ml-2">Tweet</span>
+                    </a>
+                    </li>
+                    <li class="nav-item">
+                    <a class="px-3 py-2 flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75" href="#pablo">
+                        <span class="ml-2">Settings</span>
+                    </a>
+                    </li>
                 </ul>
+                <div class="relative flex w-full sm:w-7/12 md:w-5/12 px-4 flex-wrap items-stretch lg:ml-auto">
+                    <div class="flex">
+                    <span class="font-normal leading-snug flex text-center white-space-no-wrap border border-solid border-gray-600 text-sm bg-gray-100 items-center rounded-r-none pl-2 py-1 text-pink-800 border-r-0 placeholder-pink-300">
+                        <i class="fas fa-search"></i>
+                    </span>
+                    </div>
+                    <input type="text" class="px-2 py-1 h-8 border border-solid  border-gray-600 text-sm leading-snug text-gray-700 bg-gray-100 shadow-none outline-none focus:outline-none w-full font-normal rounded-l-none flex-1 border-l-0 placeholder-pink-300" placeholder="Search pink" />
+                </div>
+                </div>
             </div>
-            """, a=wp, classes=navbar_class_html)
+            </nav>
+            """, a=wp)
         return wp
 
 jp.justpy(MUS_Navbar().navbar)
