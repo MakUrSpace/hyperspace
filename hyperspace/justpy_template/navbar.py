5f236d3b5a074d2a68107f7b4d67ef8a23509388
ssmith01850 <ssmith01850@gmail.com>
2021-12-18 11:01:33 -0500

    0009 - Updated the navbar. Changed the placement of the search bar. Also included a link for the bounty board and signing up

--- hyperspace/justpy_template/navbar.py
+++ hyperspace/justpy_template/navbar.py
@@ -9,7 +9,7 @@ class MUS_Navbar(jp.Div):
             <div class="container px-4 mx-auto flex flex-wrap items-center justify-between">
                 <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
                 <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
-                    pink Color
+                    MakUrSpace
                 </a>
                 <button class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
                     <span class="block relative w-6 h-px rounded-sm bg-white"></span>
@@ -17,6 +17,14 @@ class MUS_Navbar(jp.Div):
                     <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
                 </button>
                 </div>
+                <div class="relative flex w-full sm:w-7/12 md:w-5/12 px-4 flex-wrap items-stretch lg:ml-auto">
+                    <div class="flex">
+                    <span class="font-normal leading-snug flex text-center white-space-no-wrap border border-solid border-gray-600 text-sm bg-gray-100 items-center rounded-r-none pl-2 py-1 text-pink-800 border-r-0 placeholder-pink-300">
+                        <i class="fas fa-search"></i>
+                    </span>
+                    </div>
+                    <input type="text" class="px-2 py-1 h-8 border border-solid  border-gray-600 text-sm leading-snug text-gray-700 bg-gray-100 shadow-none outline-none focus:outline-none w-full font-normal rounded-l-none flex-1 border-l-0 placeholder-orange-300" placeholder="Search Bounties" />
+                </div>       
                 <div class="lg:flex flex-grow items-center" id="example-navbar-warning">
                 <ul class="flex flex-col lg:flex-row list-none mr-auto">
                     <li class="nav-item">
@@ -35,15 +43,28 @@ class MUS_Navbar(jp.Div):
                     </a>
                     </li>
                 </ul>
-                <div class="relative flex w-full sm:w-7/12 md:w-5/12 px-4 flex-wrap items-stretch lg:ml-auto">
-                    <div class="flex">
-                    <span class="font-normal leading-snug flex text-center white-space-no-wrap border border-solid border-gray-600 text-sm bg-gray-100 items-center rounded-r-none pl-2 py-1 text-pink-800 border-r-0 placeholder-pink-300">
-                        <i class="fas fa-search"></i>
-                    </span>
-                    </div>
-                    <input type="text" class="px-2 py-1 h-8 border border-solid  border-gray-600 text-sm leading-snug text-gray-700 bg-gray-100 shadow-none outline-none focus:outline-none w-full font-normal rounded-l-none flex-1 border-l-0 placeholder-pink-300" placeholder="Search Bounties" />
                 </div>
+                <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+                <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+                    Bounty Board
+                </a>
+                <button class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
+                    <span class="block relative w-6 h-px rounded-sm bg-white"></span>
+                    <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+                    <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+                </button>
+                </div>                
+                <div class="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
+                <a class="text-sm font-bold leading-relaxed inline-block mr-4 py-2 whitespace-nowrap uppercase text-white" href="#pablo">
+                    Sign Up / Login
+                </a>
+                <button class="cursor-pointer text-xl leading-none px-3 py-1 border border-solid border-transparent rounded bg-transparent block lg:hidden outline-none focus:outline-none" type="button">
+                    <span class="block relative w-6 h-px rounded-sm bg-white"></span>
+                    <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+                    <span class="block relative w-6 h-px rounded-sm bg-white mt-1"></span>
+                </button>
                 </div>
+
             </div>
             </nav>
             """, a=wp)
