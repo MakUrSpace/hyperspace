01560c4e1c7e42504fee3d87bec4f4aad4e09b0d
ssmith01850 <ssmith01850@gmail.com>
2022-01-21 14:45:01 -0500

    0013 - Removed the header for the original demo bounty board

--- hyperspace/justpy_template/bounty_body.py
+++ hyperspace/justpy_template/bounty_body.py
@@ -6,37 +6,11 @@ class MUS_Bounty_Board(jp.Div):
         wp = jp.WebPage()
         bounty_board_html = jp.parse_html("""
 <!-- component -->
-<link rel="stylesheet" href="https://cdn.tailgrids.com/tailgrids-fallback.css" />
 
 
 <!-- ====== Blog Section Start -->
 <section class="pt-20 lg:pt-[120px] pb-10 lg:pb-20">
    <div class="container">
-      <div class="flex flex-wrap justify-center -mx-4">
-         <div class="w-full px-4">
-            <div class="text-center mx-auto mb-[60px] lg:mb-20 max-w-[510px]">
-               <span class="font-semibold text-lg text-primary mb-2 block">
-               Our Blogs
-               </span>
-               <h2
-                  class="
-                  font-bold
-                  text-3xl
-                  sm:text-4xl
-                  md:text-[40px]
-                  text-dark
-                  mb-4
-                  "
-                  >
-                  Our Recent News
-               </h2>
-               <p class="text-base text-body-color">
-                  There are many variations of passages of Lorem Ipsum available
-                  but the majority have suffered alteration in some form.
-               </p>
-            </div>
-         </div>
-      </div>
       <div class="flex flex-wrap -mx-4">
          <div class="w-full md:w-1/2 lg:w-1/3 px-4">
             <div class="max-w-[370px] mx-auto mb-10">
